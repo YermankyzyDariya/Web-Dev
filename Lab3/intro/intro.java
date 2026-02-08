159e5e9f0caee99d98ffd371e9f80db92dbb56a9
YermankyzyDariya <ermankyzydaria@gmail.com>
2026-02-08 19:53:32 +0500

    java

---
+++ Lab3/intro/intro.java
@@ -0,0 +1,4 @@
+The term IDE (Integrated Development Environment) refers to a powerful 
+editor with many features that usually operates on a “whole project.” 
+As the name suggests, it’s not just an editor, but a full-scale “development environment.”
+
