0b82c78b65ff47c372d7933a38dd9f34ab8cc254
YermankyzyDariya <ermankyzydaria@gmail.com>
2026-02-15 22:36:00 +0500

    done

---
+++ Lab4/src/app/app.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` Hello `,
+  styles: `
+    :host {
+      color: blue;
+    }
+  `,
+})
+export class App {}
