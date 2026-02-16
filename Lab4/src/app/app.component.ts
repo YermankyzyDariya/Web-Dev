b1f877597dab200e3154536ae88c4154c9aa9761
YermankyzyDariya <ermankyzydaria@gmail.com>
2026-02-16 17:31:45 +0500

    done

--- Lab4/src/app/app.component.ts
+++ Lab4/src/app/app.component.ts
@@ -1,13 +1,392 @@
 
-import {Component} from '@angular/core';
+import {Component , output , inject} from '@angular/core';
+import {Comments} from './comments';
+import {NgOptimizedImage} from '@angular/common';
+import {Routes} from '@angular/router';
+import {Home} from './home/home';
+import {RouterOutlet, RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {FormGroup, FormControl} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Injectable} from '@angular/core';
+import {LowerCasePipe} from '@angular/common';
+import {DecimalPipe, DatePipe, CurrencyPipe} from '@angular/common';
+import {ReversePipe} from './reverse.js';
 
 @Component({
   selector: 'app-root',
-  template: ` Hello `,
+  template: ` Hello Universe  {{ city }}, {{ 1 + 1 }}  
+  Username: {{ username }}`,
   styles: `
     :host {
-      color: blue;
+      color: #a144eb;
     }
   `,
 })
-export class App {}
+export class User {
+
+  username = 'youngTech';
+  logoUrl = '/logo.svg';
+  logoAlt = 'Angular logo';
+  favoriteFramework = '';
+
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
+  
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section>
+      <app-user />
+    </section>
+  `,
+  imports: [User],
+})
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @if (isServerRunning) {
+      <span>Yes, the server is running</span>
+    } @else {
+      <span>No, the server is not running</span>
+    }
+  `,
+})
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
+    }
+  `,
+})
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.css'],
+  template: ` <div [contentEditable]="isEditable"></div> `,
+})
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section (mouseover)="showSecretMessage()">
+      There's a secret message for you, hover to reveal:
+      {{ message }}
+    </section>
+  `,
+})
+
+@Component({
+  selector: 'app-user',
+  template: ` <p>The user's name is {{ name() }}</p> `,
+})
+
+
+
+@Component({
+  selector: 'app-child',
+  styles: `
+    .btn {
+      padding: 5px;
+    }
+  `,
+  template: ` <button class="btn" (click)="addItem()">Add Item</button> `,
+})
+export class Child {
+  readonly addItemEvent = output<string>();
+
+  addItem() {
+    this.addItemEvent.emit('🐢');
+  }
+}
+
+
+Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <h1>How I feel about Angular</h1>
+      <article>
+        <p>
+          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+          feature that makes defer loading content the easiest and most ergonomic it could possibly
+          be. The Angular community is also filled with amazing contributors and experts that create
+          excellent content. The community is welcoming and friendly, and it really is the best
+          community out there.
+        </p>
+        <p>
+          I can't express enough how much I enjoy working with Angular. It offers the best developer
+          experience I've ever had. I love that the Angular team puts their developers first and
+          takes care to make us very happy. They genuinely want Angular to be the best framework it
+          can be, and they're doing such an amazing job at it, too. This statement comes from my
+          heart and is not at all copied and pasted. In fact, I think I'll say these exact same
+          things again a few times.
+        </p>
+        <p>
+          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+          feature that makes defer loading content the easiest and most ergonomic it could possibly
+          be. The Angular community is also filled with amazing contributors and experts that create
+          excellent content. The community is welcoming and friendly, and it really is the best
+          community out there.
+        </p>
+        <p>
+          I can't express enough how much I enjoy working with Angular. It offers the best developer
+          experience I've ever had. I love that the Angular team puts their developers first and
+          takes care to make us very happy. They genuinely want Angular to be the best framework it
+          can be, and they're doing such an amazing job at it, too. This statement comes from my
+          heart and is not at all copied and pasted. In fact, I think I'll say these exact same
+          things again a few times.
+        </p>
+        <p>
+          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+          feature that makes defer loading content the easiest and most ergonomic it could possibly
+          be. The Angular community is also filled with amazing contributors and experts that create
+          excellent content. The community is welcoming and friendly, and it really is the best
+          community out there.
+        </p>
+        <p>
+          I can't express enough how much I enjoy working with Angular. It offers the best developer
+          experience I've ever had. I love that the Angular team puts their developers first and
+          takes care to make us very happy. They genuinely want Angular to be the best framework it
+          can be, and they're doing such an amazing job at it, too. This statement comes from my
+          heart and is not at all copied and pasted.
+        </p>
+      </article>
+
+      @defer (on viewport) {
+        <comments />
+      } @placeholder {
+        <p>Future comments</p>
+      } @loading (minimum 2s) {
+        <p>Loading comments...</p>
+      }
+    </div>
+  `,
+  imports: [Comments],
+})
+
+
+Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/logo.svg" alt="Angular logo" width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
+      </li>
+    </ul>
+  `,
+  imports: [NgOptimizedImage],
+})
+
+
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: Home,
+  },
+  {
+    path: 'user',
+    title: 'App User Page',
+    component: User,
+  },
+];
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <nav>
+      <a routerLink="/">Home</a>
+      |
+      <a routerLink="/user">User</a>
+    </nav>
+    <router-outlet />
+  `,
+  imports: [RouterOutlet, RouterLink],
+})
+
+
+
+
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
+  `,
+  imports: [FormsModule],
+})
+
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Framework: {{ favoriteFramework }}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
+    <button (click)="showFramework()">Show Framework</button>
+  `,
+  imports: [FormsModule],
+})
+
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+      <input type="text" formControlName="name" />
+      <input type="email" formControlName="email" />
+      <button type="submit">Submit</button>
+    </form>
+
+    <h2>Profile Form</h2>
+    <p>Name: {{ profileForm.value.name }}</p>
+    <p>Email: {{ profileForm.value.email }}</p>
+  `,
+  imports: [ReactiveFormsModule],
+})
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <form [formGroup]="profileForm">
+      <input type="text" formControlName="name" name="name" />
+      <input type="email" formControlName="email" name="email" />
+      <button type="submit" [disabled]="!profileForm.valid">Submit</button>
+    </form>
+  `,
+  imports: [ReactiveFormsModule],
+})
+
+
+
+@Injectable({
+  providedIn: 'root',
+})
+ class CarService {
+  cars = ['Sunflower GT', 'Flexus Sport', 'Sprout Mach One'];
+
+  getCars(): string[] {
+    return this.cars;
+  }
+
+  getCar(id: number) {
+    return this.cars[id];
+  }
+}
+
+
+@Component({
+  selector: 'app-root',
+  template: ` <p>Car Listing: {{ display }}</p> `,
+})
+
+
+
+
+@Component({
+  selector: 'app-root',
+  template: ` {{ username | lowercase }} `,
+  imports: [LowerCasePipe],
+})
+
+
+
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <ul>
+      <li>Number with "decimal" {{ num | number: '3.2-2' }}</li>
+      <li>Date with "date" {{ birthday | date: 'medium' }}</li>
+      <li>Currency with "currency" {{ cost | currency }}</li>
+    </ul>
+  `,
+  imports: [DecimalPipe, DatePipe, CurrencyPipe],
+})
+
+
+
+
+
+@Component({
+  selector: 'app-root',
+  template: ` Reverse Machine: {{ word | reverse }} `,
+  imports: [ReversePipe],
+})
+
+
+
+
+
+
+
+
+
+
+export class App {
+  city = 'San Francisco';
+  isServerRunning = true;
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
+  isEditable = true;
+  message = '';
+
+
+  showSecretMessage() {
+    this.message = 'Way to go 🚀';
+  }
+
+  profileForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+  });
+
+  handleSubmit() {
+    alert(this.profileForm.value.name + ' | ' + this.profileForm.value.email);
+  }
+  carService = inject(CarService);
+
+  display = this.carService.getCars().join(' ⭐️ ');
+  username = 'yOunGTECh';
+
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+
+  word = 'You are a champion';
+
+}
---
+++ Lab4/src/app/reverse.ts
@@ -0,0 +1,17 @@
+
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+  transform(value: string): string {
+    return value.split('').reverse().join('');
+  }
+}
+
+
+
+export class Reverse {
+  
+}
